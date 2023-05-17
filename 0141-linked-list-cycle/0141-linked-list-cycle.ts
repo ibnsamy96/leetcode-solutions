b37436385ff88965239e194dc659317341848701
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-05-18 00:16:00 +0300

    Time: 79 ms (51.58%), Space: 45.8 MB (43.34%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -10,14 +10,34 @@
  * }
  */
 
-function hasCycle(head: ListNode | null): boolean {
-    const nodes :ListNode[] = []
-    
-    while(head){
-        if(nodes.includes(head)) return true
-        nodes.push(head)
-        head=head.next
+type CanBeListNode = ListNode | null
+function hasCycle(head: CanBeListNode): boolean {
+
+    if(!head?.next) return false 
+
+    let slowPointer :CanBeListNode = head    
+    let fastPointer :CanBeListNode = head
+
+    while(true){
+        slowPointer = slowPointer?.next || null
+        fastPointer = fastPointer?.next?.next?.next || null
+        if(fastPointer==null) break
+        const isCycleHappened = slowPointer==fastPointer || slowPointer?.next===fastPointer
+        if(isCycleHappened)  return true
     }
     
     return false
 };
+
+
+// function hasCycle(head: ListNode | null): boolean {
+//     const nodes :ListNode[] = []
+    
+//     while(head){
+//         if(nodes.includes(head)) return true
+//         nodes.push(head)
+//         head=head.next
+//     }
+    
+//     return false
+// };
