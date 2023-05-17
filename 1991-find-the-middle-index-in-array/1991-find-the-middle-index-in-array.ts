c89374d602d8b08fd88227b4fda0eaa86c05fffe
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-05-18 01:12:40 +0300

    Time: 81 ms (8.93%), Space: 47 MB (7.14%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.ts
@@ -0,0 +1,16 @@
+function findMiddleIndex(nums: number[]): number {
+    for(const indexStr in nums) {
+        const index = parseInt(indexStr)
+
+        const left = nums.slice(0,index)
+        const right = nums.slice(index+1,nums.length)
+        
+        if(sum(left) == sum(right)) return index
+    }
+    
+    return -1
+};
+
+function sum(arr:number[]){
+  return arr.reduce((acc,curr)=>acc+curr,0)
+}
