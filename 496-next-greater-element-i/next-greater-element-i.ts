da25878353172b4db716b30cb3bb8fecf1827bf0
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-07-27 23:48:45 +0300

    Time: 95 ms (5.49%) | Memory: 49 MB (6.04%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.ts
+++ 496-next-greater-element-i/next-greater-element-i.ts
@@ -3,12 +3,12 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
      const res = []
 
     for (const num of nums1) {
-        const indexInNums2 = nums2.findIndex(numOf2 => num == numOf2)
-        const restOfNums2 = nums2.slice(indexInNums2+1)
+        const indexInNums2 = nums2.indexOf(num)
+        const restOfNums2 = nums2.slice(indexInNums2+1) // to minimize the values to search in
 
         if(restOfNums2.length == 0 ) res.push(-1)
       
-        for (let j in restOfNums2) {
+        for (const j in restOfNums2) {
             if (restOfNums2[j] > num) {
                 res.push(restOfNums2[j])
                 break
