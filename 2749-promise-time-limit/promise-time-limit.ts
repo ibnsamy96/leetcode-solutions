a7c307c1658756ed3bd3154665e0d1a8188275fd
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-09-08 16:14:09 +0300

    Time: 65 ms (44.07%) | Memory: 42.5 MB (85.82%) - LeetSync

--- 2749-promise-time-limit/promise-time-limit.ts
+++ 2749-promise-time-limit/promise-time-limit.ts
@@ -1,25 +1,20 @@
 type Fn = (...params: any[]) => Promise<any>;
-
+ 
 function timeLimit(fn: Fn, t: number): Fn {
-    const start = Date.now()
 	return async function(...args) {
-        return new Promise((resolve,reject)=>{
-            let result : unknown
-            fn(...args).then(data=>result = data).catch(err=>reject(err))
+        return new Promise(async (resolve,reject)=>{
 
-            const interval = 1
-            let elapsedTime =  Date.now() - start
-            const intervalHolder = setInterval(()=>{
-                if(result) {
-                    resolve(result) 
-                    clearInterval(intervalHolder)
-                }
-                else if(elapsedTime>=t) {
-                    reject("Time Limit Exceeded")
-                    clearInterval(intervalHolder)
-                }
-                elapsedTime =  Date.now() - start
-            },interval)
-        })
+            // the timeLimitInterval is used to reject if the time limit is exceeded
+            const timeLimitInterval = setTimeout(()=>{
+                reject("Time Limit Exceeded")
+                clearInterval(timeLimitInterval)
+            },t)
+
+            // As long as the time limit wasn't exceeded yet, wait for fn() to fulfill and  resolve the result come out of it.
+            try { resolve( await fn(...args) ) }
+            catch (err:unknown) { reject(err) }
+            clearInterval(timeLimitInterval)
+    
+       })
     }
 };
