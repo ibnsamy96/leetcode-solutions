6587213a5e1c112106058672488bd29311e770f0
Mahmoud Samy <mahmoudsamy50@outlook.com>
2023-09-08 15:58:51 +0300

    Time: 94 ms (5.41%) | Memory: 45.9 MB (13.02%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.ts
@@ -0,0 +1,26 @@
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+    const start = Date.now()
+	return async function(...args) {
+        return new Promise((resolve,reject)=>{
+            // console.log(args)
+            let result : unknown
+            fn(...args).then(data=>result = data).catch(err=>reject(err))
+
+            const interval = 1
+            let elapsedTime =  Date.now() - start
+            const intervalHolder = setInterval(()=>{
+                if(result) {
+                    resolve(result) 
+                    clearInterval(intervalHolder)
+                }
+                else if(elapsedTime>=t) {
+                    reject("Time Limit Exceeded")
+                    clearInterval(intervalHolder)
+                }
+                elapsedTime =  Date.now() - start
+            },interval)
+        })
+    }
+};
