ad6ee393ced3297cddc7e3073a0141d5618b7346
Mahmoud Samy <mahmoudsamy50@outlook.com>
2025-01-28 00:57:56 +0200

    Update two-sum.ts

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,14 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
-    const res = []
-    for (const i in nums) {
-        const change = target - nums [i]
-        const restOfNums = nums.slice(parseInt(i)+1)
-
-        const isRestOfNumsIncludeChange = restOfNums.includes(change)
-        if (!isRestOfNumsIncludeChange) continue
-        
-        res.push(i,restOfNums.indexOf(change) + parseInt(i) + 1 )
-        break
+    const indexes = {};
+    
+    for (const stringI in nums) {
+           const i = parseInt(stringI)
+           const current = nums[i]
+           
+           const reminder = target - current
+           if(indexes.hasOwnProperty(reminder)){
+                return [ indexes[reminder] , i ]
+           }else {
+                indexes[current] = i   
+           }
     }
-    return res
 };
